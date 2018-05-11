4a5c163121330a97bdd4c8bc6df38f47ff0f800c
Murilo Avila <murilo.avila@duosystem.com>
2018-05-11 16:23:18 -0300

    Teste

--- App.js
+++ App.js
@@ -1,51 +1,23 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View} from 'react-native';
-import moment from 'moment';
-
-export default class HelloWorldApp extends Component {
-    timeNow(){
-        return moment().format('h:mm:s');
-    }
-
-    constructor(props){
-        super(props);
-        this.state = {timeNow: this.timeNow()};
-    }
-
-    componentDidMount(){
-        setInterval(() => {
-            this.setState({
-                timeNow: this.timeNow(),
-            });
-        }, 1000 );
-    }
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.timeNow}>{this.timeNow()}</Text>
-            </View>
-        );
-    }
+import React from 'react';
+import { StatusBar, Platform } from 'react-native';
+import Home from './src/screens/Home';
+//import Description from './src/screens/Description';
+import { StackNavigator } from 'react-navigation';
 
 
-}
-
-const styles = StyleSheet.create({
-    timeNow: {
-        textShadowColor: '#0AAFE6',
-        textShadowOffset: {
-            width: 0,
-            height: 0
+const App = StackNavigator({
+        Home: {
+            screen: Home
         },
-        textShadowRadius: 10,
-        fontSize: 70,
-        color: '#daf6ff'
+        // Description: {
+        //     screen: Description
+        // },
     },
-    container: {
-        flex: 1,
-        backgroundColor: '#000',
-        alignItems: 'center',
-        justifyContent: 'center'
+    {
+        cardStyle: {
+            paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight
+        }
     }
-});
+);
+
+export default App
